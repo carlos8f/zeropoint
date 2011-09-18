cba9b216c2ca6b6b1d20a92f6c1a8d6570c21923
Carlos Rodriguez <carlos@s8f.org>
2011-09-18 14:15:34 -0700

    Starting with demo, descent-like controls.

--- static/js/main.js
+++ static/js/main.js
@@ -1,47 +1,93 @@
-var camera, scene, renderer,
-geometry, material, mesh;
 
-$(function() {
-  init();
-  animate();
-});
+var statsEnabled = true;
+
+var container, stats;
+
+var camera, scene, renderer;
+
+var cross;
+
+$(init);
 
 function init() {
 
-  camera = new THREE.Camera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-  camera.position.z = 1000;
+  // scene and camera
 
   scene = new THREE.Scene();
+  scene.fog = new THREE.FogExp2( 0xffffff, 0.002 );
 
-  geometry = new THREE.CubeGeometry( 200, 200, 200 );
-  material = new THREE.MeshBasicMaterial( {
-    color: 0xffffff, 
-    wireframe: true
-  } );
+  camera = new THREE.RollCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+  camera.movementSpeed = 100;
+  camera.lookSpeed = 3;
+  camera.constrainVertical = [ -0.5, 0.5 ];
+  //camera.autoForward = true;
 
-  mesh = new THREE.Mesh( geometry, material );
-  scene.addObject( mesh );
+  // world
 
-  renderer = new THREE.CanvasRenderer();
+  var cube = new THREE.CubeGeometry( 20, 60, 20 );
+
+  cube.vertices[ 0 ].position.multiplyScalar( 0.01 );
+  cube.vertices[ 1 ].position.multiplyScalar( 0.01 );
+  cube.vertices[ 4 ].position.multiplyScalar( 0.01 );
+  cube.vertices[ 5 ].position.multiplyScalar( 0.01 );
+
+  var material =  new THREE.MeshLambertMaterial( { color:0xffffff } );
+
+  for( var i = 0; i < 500; i++ ) {
+
+    var mesh = new THREE.Mesh( cube, material );
+    mesh.position.set(( Math.random() - 0.5 ) * 1000,
+    ( Math.random() - 0.5 ) * 1000,
+    ( Math.random() - 0.5 ) * 1000 );
+
+    mesh.updateMatrix();
+    mesh.matrixAutoUpdate = false;
+    scene.addChild( mesh );
+
+  }
+
+  scene.addChild( camera );
+
+
+  // lights
+
+  light = new THREE.DirectionalLight( 0xffffff );
+  light.position.set( 1, 1, 1 );
+  scene.addChild( light );
+
+  light = new THREE.DirectionalLight( 0x002288 );
+  light.position.set( -1, -1, -1 );
+  scene.addChild( light );
+
+  light = new THREE.AmbientLight( 0x222222 );
+  scene.addChild( light );
+
+
+  // renderer
+
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setClearColorHex( 0xffffff, 1 );
   renderer.setSize( window.innerWidth, window.innerHeight );
 
   $('body').append( renderer.domElement );
 
+  if ( statsEnabled ) {
+
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '0px';
+    stats.domElement.style.zIndex = 100;
+    $('body').append( stats.domElement );
+
+  }
+
+  animate();
 }
+
 
 function animate() {
-
-  // Include examples/js/RequestAnimationFrame.js for cross-browser compatibility.
-  requestAnimationFrame( animate );
-  render();
-
-}
-
-function render() {
-
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
-
+  requestAnimationFrame(animate);
   renderer.render( scene, camera );
-
+  if ( statsEnabled) stats.update();
 }
+
